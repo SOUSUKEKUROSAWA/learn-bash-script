815e43ebaa9780feb99c7448da6aabe4701eb902
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2023-05-13 16:38:07 +0900

    Exit codes

--- functionposargu.sh
+++ functionposargu.sh
@@ -2,6 +2,15 @@
 
 showname(){
     echo Hello $1
+
+    if [ ${1,,} = "herbert" ]; then
+        return 0
+    else
+        return 1
+    fi
 }
 
-showname Herbert
+showname $1
+if [ $? -eq 1 ]; then
+    echo "Someone unknown called the function"
+fi
